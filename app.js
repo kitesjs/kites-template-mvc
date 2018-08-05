a8de571b3bdf19fead0304adf3861fb7c7726a9a
Vunb <nhubaovu@gmail.com>
2018-08-05 07:19:57 +0700

    use kites for advanced app logging

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 'use strict'
-const engine = require('@kites/engine');
+const kites = require('kites');
 
 /**
  * Init kites
  */
-var kites = engine({
-    loadConfig: true,
-    discover: true
-});
-
-kites
+module.exports = kites({
+        loadConfig: true,
+        discover: true
+    })
     .use(require('./content/extensions/detectEnv'))
     .use(require('./content/extensions/nativeModel'))
     .use(require('./content/extensions/configRoutes'))
     .init()
     .then(function (kites) {
         kites.logger.info('Info: Web Server is Ready!!!')
+        // return kites for testing.
+        return kites;
     })
     .catch(function (e) {
         console.error(e.stack);
         process.exit(1);
     });
-
-module.exports = kites;
